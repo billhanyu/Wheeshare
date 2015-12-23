7b8e1b879b87d91906e618ade282f5ae10dbf731
Bill Yu <han.yu@duke.edu>
2015-12-23 16:05:29 +0800

    thumbnail image to reduce RAM taken

--- Need&Give/Functions.swift
+++ Need&Give/Functions.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Dispatch
+import UIKit
 
 func delay(seconds seconds: Double, completion:()->()) {
     let popTime = dispatch_time(DISPATCH_TIME_NOW, Int64( Double(NSEC_PER_SEC) * seconds ))
@@ -15,4 +16,22 @@ func delay(seconds seconds: Double, completion:()->()) {
     dispatch_after(popTime, dispatch_get_main_queue()) {
         completion()
     }
+}
+
+func imageDataScaledToHeight(imageData: NSData, height: CGFloat) -> NSData {
+        
+    let image = UIImage(data: imageData)!
+    let oldHeight = image.size.height
+    let scaleFactor = height / oldHeight
+    let newWidth = image.size.width * scaleFactor
+    let newSize = CGSizeMake(newWidth, height)
+    let newRect = CGRectMake(0, 0, newWidth, height)
+    
+    UIGraphicsBeginImageContext(newSize)
+    image.drawInRect(newRect)
+    let newImage =
+    UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    
+    return UIImageJPEGRepresentation(newImage, 0.8)!
 }
